2ee739b054a19c4be9fb5ebcefb53376c2eddf1f
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-11-13 16:27:31 +0300

    telegram section

--- js/carousel-settings.js
+++ js/carousel-settings.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    //about carousel
+    //deals carousel
     var dealsOwl = $('.deals__carousel').owlCarousel({
         loop: false,
         autoplay: false,
@@ -36,5 +36,45 @@ $(document).ready(function(){
     })
     $('.deals__right-arrow').click(function() {
       dealsOwl.trigger('next.owl.carousel');
+    });
+
+
+      //reviews carousel
+      var reviewsOwl = $('.reviews__carousel').owlCarousel({
+        loop: false,
+        autoplay: false,
+        slideTransition: 'linear',
+        autoplayTimeout: 2000,
+        autoplaySpeed: 2000,
+        nav: false,
+        dots: false,
+        items: 3,
+        // responsive:{
+        //   0:{
+        //     items: 5
+        //   },
+        //   1150:{
+        //     items: 5
+        //   },
+        //   1400:{
+        //     items: 6,
+        //   },
+        //   1600:{
+        //     items: 6,
+        //   },
+        //   1800:{
+        //     items: 7,
+        //   },
+        //   2200:{
+        //     items: 8,
+        //   },
+        // }
+    });
+
+    $('.reviews__left-arrow').click(function() {
+      reviewsOwl.trigger('prev.owl.carousel');
+    })
+    $('.reviews__right-arrow').click(function() {
+      reviewsOwl.trigger('next.owl.carousel');
     })
 });
