b08c2c165b3c7c96c69f76220cd0209b25183a91
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-11-15 14:12:49 +0300

    faq & footer adaptive

--- js/carousel-settings.js
+++ js/carousel-settings.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
     });
 
 
-      //reviews carousel
+    //reviews carousel
     var reviewsOwl = $('.reviews__carousel').owlCarousel({
       loop: false,
       autoplay: false,
@@ -54,27 +54,32 @@ $(document).ready(function(){
       nav: false,
       dots: false,
       items: 3,
-      // responsive:{
-      //   0:{
-      //     items: 5
-      //   },
-      //   1150:{
-      //     items: 5
-      //   },
-      //   1400:{
-      //     items: 6,
-      //   },
-      //   1600:{
-      //     items: 6,
-      //   },
-      //   1800:{
-      //     items: 7,
-      //   },
-      //   2200:{
-      //     items: 8,
-      //   },
-      // }
+      onInitialize: reviewsInit,
+        responsive:{
+          0:{
+            items: 1,
+            stagePadding: 50,
+          },
+          425:{
+            items: 1,
+            stagePadding: 100,
+          },
+          768:{
+            items: 2,
+            stagePadding: 100,
+          },
+          1380:{
+            items: 3,
+          },
+        }
     });
+
+    function reviewsInit(){
+      if (window.innerWidth > 1360){
+        const lastItem = document.createElement('div');
+        document.querySelector('.reviews__carousel').appendChild(lastItem);
+      }
+    }
 
     $('.reviews__left-arrow').click(function() {
       reviewsOwl.trigger('prev.owl.carousel');
